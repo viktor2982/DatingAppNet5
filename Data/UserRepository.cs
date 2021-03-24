877968e15d49d0effc997d904a43ea5f6cfec586
Vitoco <vc.acunauribe@gmail.com>
2021-03-23 22:09:21 -0300

    End of section 13

--- Data/UserRepository.cs
+++ Data/UserRepository.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -30,11 +32,31 @@ namespace API.Data
         }
 
         // More Efficient
-        public async Task<IEnumerable<MemberDTO>> GetMembersAsync()
+        public async Task<PagedList<MemberDTO>> GetMembersAsync(UserParams userParams)
         {
-            return await _context.Users
-                .ProjectTo<MemberDTO>(_mapper.ConfigurationProvider)
-                .ToListAsync();
+            var query = _context.Users.AsQueryable();
+
+            // Filtering
+            query = query.Where( u => u.Username != userParams.CurrentUsername );
+            query = query.Where( u => u.Gender == userParams.Gender );
+
+            var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+            var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+            query = query.Where( u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob );
+
+            // Ordering
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending( u => u.Created ),
+                _ => query.OrderByDescending( u => u.LastActive )
+            };
+
+            return await PagedList<MemberDTO>.CreateAsync(
+                query.ProjectTo<MemberDTO>(_mapper.ConfigurationProvider).AsNoTracking(),
+                userParams.PageNumber,
+                userParams.PageSize
+            );
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
